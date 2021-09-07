84cc05e01df4017e4a461ec9c2fd75568e629773
Geovane de Sá Ferreira <geovane.portfolio@gmail.com>
2021-09-07 15:12:19 -0300

    update

--- Projeto/react1/src/index.js
+++ Projeto/react1/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Reducers from './reducers'
 import App from './App';
 import './App.css';
 
+const store = createStore(Reducers);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
